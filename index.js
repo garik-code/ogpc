a189a138759158f1b6098e82475c18cb1b34ff64
Игорь Мещеряков <maiL@garik.site>
2019-04-23 17:23:07 +0400

    %)

--- index.js
+++ index.js
@@ -27,35 +27,16 @@
   const app = express()
   const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-  app.get('/:token/:start/:end', urlencodedParser, (req, res) => Coinmarketcap.ohlcv(req.params.token, req.params.start, req.params.end)
+  app.get('/coinmarketcap/:token/:start/:end', urlencodedParser, (req, res) => Coinmarketcap.ohlcv(req.params.token, req.params.start, req.params.end)
   .then(data => res.send(data), err => {
     console.log(err)
     res.send('err get coinmarketcap')
   }))
 
-  app.get('/mifengcha/history/:token/:end', urlencodedParser, (req, res) => {
-    let proxy = proxyGet()
-    console.log(`proxy: ${proxy}`);
-    console.log(`https://data.mifengcha.com/api/v2/price/history?symbol_name=${req.params.token}&start=&end=${req.params.end}`);
-    console.log(`https://data.mifengcha.com/api/v2/price/history?symbol_name=${req.params.token}&start=&end=${req.params.end}`)
-    request.get({
-        url: `https://data.mifengcha.com/api/v2/price/history?symbol_name=${req.params.token}&start=${req.params.start}&end=${req.params.end}`,
-        // proxy: `socks5://${proxy}`
-    }, (err, page) => {
-        if (err) {
-            res.send(err)
-        } else {
-            let table = ''
-            let arr = JSON.parse(page.body)
-            for (let i = 0; i <= arr.length; i++) {
-              if(i == arr.length){
-                res.send(`<table>${table}</table>`)
-              }else{
-                table += `<tr><td>${arr[i][1]}</td><td>${arr[i][2]}</td><td>${arr[i][3]}</td><td>${arr[i][4]}</td></tr>`
-              }
-            }
-        }
-    })
-  })
+  app.get('/mifengcha/:token/:start/:end', urlencodedParser, (req, res) => Mifengcha.ohlcv(req.params.token, req.params.start, req.params.end)
+  .then(data => res.send(data), err => {
+    console.log(err)
+    res.send('err get coinmarketcap')
+  }))
 
   app.listen(3123)
