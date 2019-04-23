40c4176c74098c88882935a908f5b569cfed02f2
Игорь Мещеряков <mail@garik.site>
2019-04-23 05:12:51 +0400

    mifengcha

--- index.js
+++ index.js
@@ -62,4 +62,29 @@ app.get('/:token/:start/:end', urlencodedParser, (req, res) => {
   })
 })
 
+app.get('/mifengcha/history/:token/:end', urlencodedParser, (req, res) => {
+  let proxy = proxyGet()
+  console.log(`proxy: ${proxy}`);
+  console.log(`https://data.mifengcha.com/api/v2/price/history?symbol_name=${req.params.token}&start=&end=${req.params.end}`);
+  console.log(`https://data.mifengcha.com/api/v2/price/history?symbol_name=${req.params.token}&start=&end=${req.params.end}`)
+  request.get({
+      url: `https://data.mifengcha.com/api/v2/price/history?symbol_name=${req.params.token}&start=${req.params.start}&end=${req.params.end}`,
+      proxy: `socks5://${proxy}`
+  }, (err, page) => {
+      if (err) {
+          res.send(err)
+      } else {
+          let table = ''
+          let arr = JSON.parse(page.body)
+          for (let i = 0; i <= arr.length; i++) {
+            if(i == arr.length){
+              res.send(`<table>${table}</table>`)
+            }else{
+              table += `<tr><td>${arr[i][1]}</td><td>${arr[i][2]}</td><td>${arr[i][3]}</td><td>${arr[i][4]}</td></tr>`
+            }
+          }
+      }
+  })
+})
+
 app.listen(3123)
